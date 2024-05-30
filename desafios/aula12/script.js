57e6956fe9fe3c78ac41c65d00eb80996a48d155
Gustavo Capelli <gustavocapelli20092005@gmail.com>
2024-05-30 10:30:03 -0300

    Update script.js

--- desafios/aula12/script.js
+++ desafios/aula12/script.js
@@ -1,5 +1,17 @@
-var msg = window.document.getElementById('msg')
-var img = window.document.getElementById('img')
-var data = new Date()
-var hora = data.getHours()
-msg.innerHTML = `Agora são ${hora} horas.`
+function carregar(){
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('img')
+    var data = new Date()
+    //var hora = data.getHours()
+    var hora = 17
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12){
+        img.src = 'manha-nova.png'
+    }
+    else if (hora >= 12 && hora < 18){
+        img.src = 'tarde-nova.png'
+    }
+    else{
+        img.src = 'noite-nova.png'
+    }
+}
