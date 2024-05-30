6686f6566481517ff7dfbc5302a59ce0cd43066a
Gustavo Capelli <gustavocapelli20092005@gmail.com>
2024-05-30 17:06:52 -0300

    up

--- desafios/aula12/d002/script.js
+++ desafios/aula12/d002/script.js
@@ -3,12 +3,44 @@ function verificar(){
     var ano = data.getFullYear()
     var fano = window.document.getElementById('txtano')
     var res = window.document.getElementById('res')
-    if (fano.ariaValueMax.length == 0 || Number(fano.value) > ano){
+    if (fano.value.length == 0 || Number(fano.value) > ano){
         window.alert('ERRO! Verifique os dados e tente novamente.')
     }
     else{
         var fsex = window.document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
-        res.innerHTML = `Idade calculada: ${idade}`
+        var gen = ''
+        var img = window.document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked){
+            gen = 'homem'
+            if (idade >= 0 && idade < 10){
+                img.setAttribute('src', 'imagens/criaca-h.png')
+            } else if (idade < 20){
+                img.setAttribute('src', 'imagens/jovem-h.png')
+            } else if(idade < 40){
+                img.setAttribute('src', 'imagens/homem.png')
+            } else if (idade < 60){
+                img.setAttribute('src', 'imagens/homem-mais-velho.png')
+            } else {
+                img.setAttribute('src', 'imagens/velho.png')
+            }
+        }
+        else if (fsex[1].checked){
+            gen = 'mulher'
+            if (idade >= 0 && idade < 10){
+                img.setAttribute('src', 'imagens/crianca-m.png')
+            } else if (idade < 20){
+                img.setAttribute('src', 'imagens/jovem-m.png')
+            } else if(idade < 40){
+                img.setAttribute('src', 'imagens/mulher.png')
+            } else if (idade < 60){
+                img.setAttribute('src', 'imagens/mulher-mais-velha.png')
+            } else {
+                img.setAttribute('src', 'imagens/velha.png')
+            }
+        }
+        res.innerHTML = `Detectamos um(a) ${gen} de ${idade} anos.`
+        res.appendChild(img)
     }
 }
