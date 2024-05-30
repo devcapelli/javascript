6d858e0cca5753098df5347d801df57dc75bd041
Gustavo Capelli <gustavocapelli20092005@gmail.com>
2024-05-30 11:19:21 -0300

    finalizando d001

--- desafios/aula12/script.js
+++ desafios/aula12/script.js
@@ -2,16 +2,18 @@ function carregar(){
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('img')
     var data = new Date()
-    //var hora = data.getHours()
-    var hora = 17
+    var hora = data.getHours()
     msg.innerHTML = `Agora são ${hora} horas.`
     if (hora >= 0 && hora < 12){
-        img.src = 'manha-nova.png'
+        img.src = 'imagens/manha-nova.png'
+        document.body.style.backgroundColor = '#D7A65E'
     }
     else if (hora >= 12 && hora < 18){
-        img.src = 'tarde-nova.png'
+        img.src = 'imagens/tarde-nova.png'
+        document.body.style.backgroundColor = '#F26D3D'
     }
     else{
-        img.src = 'noite-nova.png'
+        img.src = 'imagens/noite-nova.png'
+        document.body.style.backgroundColor = '#0C1531'
     }
 }
