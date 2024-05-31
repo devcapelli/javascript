b19c1f6c6806aa30a02fb077ac800480f2e834f0
Gustavo Capelli <gustavocapelli20092005@gmail.com>
2024-05-31 10:16:28 -0300

    up

--- exercicios/aula12/ex012.js
+++ exercicios/aula12/ex012.js
@@ -1,15 +1,11 @@
-var agora = new Date()
-var hora = agora.getHours()
-console.log(`Agora são exatamente ${hora} horas.`)
-if (hora < 6){
-    console.log('Boa Madrugada!')
+var idade = 18
+console.log(`Você tem ${idade} anos.`)
+if (idade < 16){
+    console.log('Não vota')
 }
-else if (hora <= 12){
-    console.log('Bom Dia!')
+else if ( idade < 18 || idade > 65){
+    console.log('Voto opcional')
 }
-else if (hora <= 18){
-    console.log('Boa Tarde!')
-}
-else if(hora <= 24){
-    console.log('Boa Noite!')
+else{
+    console.log('Voto obrigatório')
 }
--- exercicios/aula12/ex013.js
+++ exercicios/aula12/ex013.js
@@ -1,28 +1,15 @@
 var agora = new Date()
-var diaSem = agora.getDay()
-console.log(diaSem)
-switch(diaSem){
-    case 0:
-        console.log('Domingo')
-        break
-    case 1:
-        console.log('Segunda-Feira')
-        break
-    case 2:
-        console.log('Terça-Feira')
-        break
-    case 3:
-        console.log('Quarta-Feira')
-        break
-    case 4:
-        console.log('Quinta-Feira')
-        break
-    case 5:
-        console.log('Sexta-Feira')
-        break
-    case 6:
-        console.log('Sábado')
-        break
-    default:
-        console.log('[ERRO] Dia inválido!')
+var hora = agora.getHours()
+console.log(`Agora são exatamente ${hora} horas.`)
+if (hora < 6){
+    console.log('Boa Madrugada!')
+}
+else if (hora <= 12){
+    console.log('Bom Dia!')
+}
+else if (hora <= 18){
+    console.log('Boa Tarde!')
+}
+else if(hora <= 24){
+    console.log('Boa Noite!')
 }
