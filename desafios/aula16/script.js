392ead73cf8936c1053707f65e46667c1c686734
Gustavo Capelli <gustavocapelli20092005@gmail.com>
2024-06-07 11:02:16 -0300

    finalizando o desafio

--- desafios/aula16/script.js
+++ desafios/aula16/script.js
@@ -1,5 +1,5 @@
 var num = document.getElementById('txtn')
-var l = document.getElementById('flista')
+var lista = document.getElementById('flista')
 var res = document.getElementById('res')
 var valores = []
 
@@ -11,7 +11,7 @@ function isNumero(n){
     }
 }
 
-function isLista(n, l){
+function inLista(n, l){
     if(l.indexOf(Number(n)) != -1) {
         return true
     } else{
@@ -20,13 +20,40 @@ function isLista(n, l){
 }
 
 function adicionar(){
-    if(isNumero(num.value) && !inLista(num.value, valores)){
-
+    if(isNumero(num.value) && !inLista(num.value, valores)) {
+        valores.push(Number(num.value))
+        var item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado.`
+        lista.appendChild(item)
     } else{
-        window.alert('Valor inválido ou já adicionado na lista.')
+        window.alert('Valor inválido ou já adicionado.')
     }
+    num.value = ''
+    num.focus()
 }
 
 function finalizar(){
-
+    if(valores.length == 0){
+        window.alert('Adicione valores antes de finalizar.')
+    } else{
+        var tot = valores.length
+        var maior = valores[0]
+        var menor = valores[0]
+        var soma = 0
+        var media = 0
+        for(var pos in valores){
+            soma += valores[pos]
+            if(valores[pos] > maior)
+                maior = valores[pos]
+            if(valores[pos] < menor)
+                menor = valores[pos]
+        }
+        media = soma / tot
+        res.innerHTML = ''
+        res.innerHTML += `Ao todo, temos ${tot} números cadastrados.</p>`
+        res.innerHTML += `<p>O maior valor adicionado foi ${maior}.</p>`
+        res.innerHTML += `<p>O menor valor adicionado foi ${menor}.</p>`
+        res.innerHTML += `<p>A soma de todos os valores é ${soma}.</p>`
+        res.innerHTML += `<p>A média dos valores é ${media}.</p>`
+    }
 }
